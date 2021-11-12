fb1b0de6b54dd55dc6a63758005ee6a43b477932
anna sganderla <sganderlakarlaanna@gmail.com>
2021-11-12 16:00:52 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'app-root',  // tag html
+  templateUrl: './app.component.html',  // conteúdo da tag
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
